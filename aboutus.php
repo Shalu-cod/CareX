6a73a458c3280651c120d6b3081d1ee7d3450551
Shalu-cod <76805482+Shalu-cod@users.noreply.github.com>
2021-05-06 13:41:00 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Covid Warriors</title>
+  <link rel="stylesheet" type="text/css" href="democss/demostyle.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
+</head>
+<section>
+	<div>
+		<h2 align="center"><b>Data N' Hack Elite</b></h2>
+	</div>	
+	<br>
+	<br>
+				<div >
+				    <div class="tab1cards">
+						
+						<div class="card">
+						  <img src="images_frost hack1/geetanshu.png" alt="geetanshu" style="width:100%">
+						  <div class="container">
+						    <h4><b>Geetashu Arsiya</b></h4> 
+						    <p>Electrical Engineer </p> 
+						    <a href="#" class="btn btn-outline-success">See Profile</a>
+						 
+						  </div>
+					    </div>
+					    
+						<div class="card">
+						  <img src="images_frost hack1/janani.png" alt="Janani" style="width:100%">
+						  <div class="container">
+						    <h4><b>Janani</b></h4> 
+						    <p>Electrical Engineer</p> 
+						    <a href="#" class="btn btn-outline-success">See Profile</a>
+						  </div>
+						</div>
+					    
+						<div class="card">
+						  <img src="images_frost hack1/Shalu.jpg" alt="Shalu" style="width:100%">
+						  <div class="container">
+						    <h4><b>Shalu</b></h4> 
+						    <p>Electrical Engineer</p> 
+						    <a href="#" class="btn btn-outline-success">See Profile</a>
+						  </div>
+						</div>
+					 
+		            </div>
+	            </div>
+</section>	
+
